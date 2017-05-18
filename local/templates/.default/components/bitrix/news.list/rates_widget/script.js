f7f05726191e87ce96367284a83c928068c18471
Furin Konstantin <furin.konstantin1988@gmail.com>
2017-05-18 16:35:22 +0400

    Внесение правок в шаблон 16

--- local/templates/.default/components/bitrix/news.list/rates_widget/script.js
+++ local/templates/.default/components/bitrix/news.list/rates_widget/script.js
@@ -1,6 +1,6 @@
 $(function() {
     $(document).on("click", "#feedback_rate", function(e) {
         e.preventDefault();
-        $("#MESSAGE").val($(this).closest(".pricing-plan").find(".price-title span").eq(0).text());
+        $("#MESSAGE").val($(this).closest(".pricing-plan").find(".price-title span").eq(0).text() + ".");
     });
 });
